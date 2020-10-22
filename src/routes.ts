d75665cccc1e5d50c8dbddafa8c025d1490f6f43
Lucas Jantsch Guedes <llucas.jguedes@gmail.com>
2020-10-22 19:50:53 -0300

    corrigindo comentário

--- src/routes.ts
+++ src/routes.ts
@@ -6,7 +6,7 @@ const routes = Router();
 // Rota de listagem de usuários
 routes.get('/users', UserController.index);
 
-// Rora de listagem de um unico usuário
+// Rota de listagem de um unico usuário
 routes.get('/users/:id', UserController.show);
 
 // Rota de criação de usuário
